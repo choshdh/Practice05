747d5cfe0d57cafe6854b18a2bd60cad26798199
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 20:32:43 +0900

    1번완료

--- src/com/javaex/problem01/CalcApp.java
+++ src/com/javaex/problem01/CalcApp.java
@@ -1,8 +1,67 @@
 package com.javaex.problem01;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("사칙 연산 계산기 프로그램을 실행합니다.");
+    	System.out.println("구분자로 공백을 사용 합니다.");
+    	System.out.println("계산 할 식을 넣어주세요. 예) a + b");
+    	while(true) {
+    		System.out.print("식 입력>> ");
+    		String get = sc.nextLine();
+    		
+    		String[] getSplit = get.split(" ");
+			
+    		if(getSplit.length==3) {
+    			int numA;
+    			int numB;
+    			try {
+    				numA = Integer.parseInt(getSplit[0]);
+    				numB = Integer.parseInt(getSplit[2]);
+    				if(getSplit[0].equals("/q")) {
+    	    			break;
+    	    		}else if(getSplit[1].equals("+")) {
+    	    			Add a = new Add();
+    	    			a.setValue(numA,numB);
+    	    			System.out.println(a.calculate());
+    	    		}else if(getSplit[1].equals("-")) {
+    	    			Sub s = new Sub();
+    	    			s.setValue(numA,numB);
+    	    			System.out.println(s.calculate());
+    	    		}else if(getSplit[1].equals("*")) {
+    	    			Mul m = new Mul();
+    	    			m.setValue(numA,numB);
+    	    			System.out.println(m.calculate());
+    	    		}else if(getSplit[1].equals("/")) {
+    	    			Div d = new Div();
+    	    			d.setValue(numA,numB);
+    	    			System.out.println(d.calculate());
+    	    		}else {
+    	    			System.out.println("알 수 없는 연산자 입니다.");
+    	    			System.out.println("입력된 연산자 : " + getSplit[1]);
+    	    		}
+    			} catch (NumberFormatException e) {
+    				// TODO Auto-generated catch block
+    				System.out.println("피연산자가 잘못 입력 되었습니다.");
+    				System.out.println("입력된 피연산자 a : " + getSplit[0] + " 피연산자 b : " + getSplit[2]);
+    				continue;
+    			}
+			}else if(get.equals("/q")) {
+				break;
+			}else{
+				System.out.println("피연산자 2개와 연산자 1개를 정확히 입력해주십시오.");
+				continue;
+			}		
+    		
+    	}
+    	
+    	System.out.println("계산기가 종료되었습니다..");
+    	
+    	
     }
 }
--- src/com/javaex/problem01/Sub.java
+++ src/com/javaex/problem01/Sub.java
@@ -2,5 +2,14 @@ package com.javaex.problem01;
 
 public class Sub {
 
+	int a,b;
+	
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
 
+	public int calculate() {
+		return a-b;
+	}
 }
