db6ec0fbbe7668619b16ddc9d3d7560304a59081
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-10 08:01:54 +0900

    2번 완료

--- src/com/javaex/problem02/FriendApp.java
+++ src/com/javaex/problem02/FriendApp.java
@@ -1,10 +1,33 @@
 package com.javaex.problem02;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FriendApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
+    	
+    	ArrayList <Friend> al = new ArrayList<Friend>();
        
+    	for(int i=0; i<3; i++) {
+        	String friendInfo = sc.nextLine();
+        	// 입력받은 친구정보를 공백으로 분리
+        	String[] friend = friendInfo.split(" ");
+        	// Friend 객체 생성하여 데이터 넣기
+        	Friend f = new Friend();
+        	f.setName(friend[0]);
+        	f.setHp(friend[1]);
+        	f.setSchool(friend[2]);
+        	// 배열에 추가하기
+        	al.add(f);
+        }
+    	
+    	for(Friend f : al) {
+    		f.showInfo();
+    	}
+    	
     }
 
 }
