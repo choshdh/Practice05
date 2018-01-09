f87aea76f61b815c47bbf464c0c1dc48005ae960
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-10 08:29:09 +0900

    수정

--- src/com/javaex/problem03/Goods.java
+++ src/com/javaex/problem03/Goods.java
@@ -6,6 +6,22 @@ public class Goods {
     private int price;
     private int count;
     
+	public void showInfo() {
+		System.out.println(name +"(가격:" + price + "원)이 " + count + "개 입고 되었습니다.");
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+    
     
     
 }
--- src/com/javaex/problem03/GoodsApp.java
+++ src/com/javaex/problem03/GoodsApp.java
@@ -8,6 +8,36 @@ public class GoodsApp {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
+    	
+    	ArrayList <Goods> al = new ArrayList<Goods>();
+    	
+    	System.out.println("재고관리 프로그램을 실행 합니다.");
+    	System.out.println("구분자로 공백 한칸을 사용 합니다.");
+    	System.out.println("상품 정보를 넣어주세요 예)사이다 500 10");
+        
+    	while(al.size()<3) {
+    		System.out.println("상품 정보 입력 >>");
+        	String goodsInfo = sc.nextLine();
+        	
+        	String[] goods = goodsInfo.split(" ");
+        	
+        	Goods g = new Goods();
+        	g.setName(goods[0]);
+        	try {
+				g.setPrice(Integer.parseInt(goods[1]));
+				g.setCount(Integer.parseInt(goods[2]));
+				al.add(g);
+			} catch (NumberFormatException e) {
+				System.out.println("가격 및 수량에는 숫자만 기입 할 수 있습니다.");
+				continue;
+			}
+        	
+        }
+    	
+    	for(Goods g : al) {
+    		g.showInfo();
+    	}
        
     }
 
